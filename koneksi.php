736bcba77d212b4d070a9fbe287e0f08d80c71cc
Giant RJ <giant.r.j@gmail.com>
2023-12-04 11:26:35 +0700

    comment database settings

--- koneksi.php
+++ koneksi.php
@@ -1,8 +1,12 @@
 <?php
 require './libs/rb.php';
 
-R::setup( 'mysql:host=localhost;dbname=dbname',
-        'root', '' );
+/**
+ * uncomment this for database connection
+ * fill the params with correct database for your project
+ * 
+R::setup( 'mysql:host=localhost;dbname=dbname', 'root', '' );
 
 if(!R::testConnection()) die('No DB connection!');
+*/
 ?>
